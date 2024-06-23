531105d968eaf066173332d0e6565e9d719e06e2
PPHelios <pphelios1@gmail.com>
2024-06-23 10:34:56 +0300

    Fix: contact escape character

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { MovingBorderButton } from "./ui/MovingBorderButton";
-import { ArrowUpRight } from 'lucide-react';
+import { ArrowUpRight } from "lucide-react";
 
 function Contact() {
   return (
@@ -10,19 +10,21 @@ function Contact() {
     >
       <div
         className="ml-auto mr-auto flex max-w-3xl flex-col items-center
-          justify-center gap-8 py-40 px-5"
+          justify-center gap-8 px-5 py-40"
       >
         <h2 className="text-center text-4xl md:text-6xl">
           Ready to take{" "}
           <span className="font-bold text-emerald-600">your </span>
           digital presence to the next level?
         </h2>
-        <p className="text-xl text-center md:text-left">
-          Reach out to me, and let's discuss how can I help you to achieve your
-          goals.
+        <p className="text-center text-xl md:text-left">
+          Reach out to me, and let&apos;s discuss how can I help you to achieve
+          your goals.
         </p>
         <a href="mailto:haytham.atef1@gmail.com">
-          <MovingBorderButton>Contact me now <ArrowUpRight/> </MovingBorderButton>
+          <MovingBorderButton>
+            Contact me now {" "}<ArrowUpRight />
+          </MovingBorderButton>
         </a>
       </div>
     </section>
